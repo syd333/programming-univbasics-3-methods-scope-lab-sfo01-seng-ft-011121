513b4a25272fa35be4ce4f730a717153e3bc97bb
sydney miranda <sydmiranda@icloud.com>
2020-12-17 16:45:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
-  $ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
